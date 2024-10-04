e165eea5c2da7af958d7cbf6c4ddfd581ad8eb55
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-10-04 12:20:53 -0400

    Add project template for buttonbox-lib and repository config files

---
+++ buttonbox-lib/src/main/java/Program.java
@@ -0,0 +1,32 @@
+
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTablesJNI;
+import edu.wpi.first.util.CombinedRuntimeLoader;
+
+import java.io.IOException;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+
+import edu.wpi.first.cscore.CameraServerCvJNI;
+import edu.wpi.first.cscore.CameraServerJNI;
+import edu.wpi.first.math.WPIMathJNI;
+import edu.wpi.first.util.WPIUtilJNI;
+
+/**
+ * Program
+ */
+public class Program {
+    public static void main(String[] args) throws IOException {
+        NetworkTablesJNI.Helper.setExtractOnStaticLoad(false);
+        WPIUtilJNI.Helper.setExtractOnStaticLoad(false);
+        WPIMathJNI.Helper.setExtractOnStaticLoad(false);
+        CameraServerJNI.Helper.setExtractOnStaticLoad(false);
+        CameraServerCvJNI.Helper.setExtractOnStaticLoad(false);
+
+        CombinedRuntimeLoader.loadLibraries(Program.class, "wpiutiljni", "wpimathjni", "ntcorejni", Core.NATIVE_LIBRARY_NAME, "cscorejni");
+
+        var inst = NetworkTableInstance.getDefault();
+        Mat m = new Mat();
+    }
+}
