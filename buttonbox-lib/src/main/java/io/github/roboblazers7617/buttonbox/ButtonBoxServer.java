27784c28f4f83ce119f85380f906372f0f21a6ff
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-10-06 15:32:01 -0400

    Add some NetworkTables test code

--- buttonbox-lib/src/main/java/io/github/roboblazers7617/buttonbox/ButtonBoxServer.java
+++ buttonbox-lib/src/main/java/io/github/roboblazers7617/buttonbox/ButtonBoxServer.java
@@ -1,17 +1,27 @@
 package io.github.roboblazers7617.buttonbox;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 /**
  * Class for robot-side ButtonBox logic.
  */
 public class ButtonBoxServer extends SubsystemBase {
+	private final DoublePublisher valuePub;
+	private double value = 0;
+
 	/** Creates a new ButtonBoxServer. */
 	public ButtonBoxServer() {
+		NetworkTableInstance inst = NetworkTableInstance.getDefault();
+		NetworkTable table = inst.getTable("ButtonBox");
+		valuePub = table.getDoubleTopic("value").publish();
 	}
 
 	@Override
 	public void periodic() {
-		System.out.println("Test!");
+		value += 0.01;
+		valuePub.set(value);
 	}
 }
