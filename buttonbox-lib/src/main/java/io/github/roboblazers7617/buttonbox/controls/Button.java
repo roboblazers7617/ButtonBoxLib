eb0ab81239510d5030a37717dbd4d397de41b277
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-10-07 18:04:06 -0400

    Add a Button Control Currently there's not much client-side logic in this, but the server-side logic is pretty much finished.

---
+++ buttonbox-lib/src/main/java/io/github/roboblazers7617/buttonbox/controls/Button.java
@@ -0,0 +1,120 @@
+package io.github.roboblazers7617.buttonbox.controls;
+
+import io.github.roboblazers7617.buttonbox.Control;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.BooleanTopic;
+import edu.wpi.first.networktables.BooleanPublisher;
+import edu.wpi.first.networktables.BooleanSubscriber;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+public class Button extends Control {
+	/** Used to communicate whether the physical button is pressed or not. */
+	private BooleanTopic pressedTopic;
+	private BooleanPublisher pressedPub;
+	private BooleanSubscriber pressedSub;
+
+	/** Used to communicate whether the button is active or not. */
+	private BooleanTopic stateTopic;
+	private BooleanPublisher statePub;
+	private BooleanSubscriber stateSub;
+
+	/** Stores whether the physical button is pressed or not. */
+	private boolean pressed;
+	/** Stores whether the physical button was pressed or not on the last update. Used to determine rising/falling edges. */
+	private boolean lastPressed;
+	/** Stores the state of the button on the server, which is updated according to the button's mode. */
+	private boolean state;
+	/** Stores the selected {@link ButtonMode}. */
+	private ButtonMode buttonMode = ButtonMode.PUSH;
+
+	/**
+	 * Determines how the state of the button should be updated when the button is pressed or released.
+	 */
+	public enum ButtonMode {
+		/** Enabled when pressed, disabled when released. */
+		PUSH,
+		/** Toggle button, triggers on the rising edge. */
+		TOGGLE_RISING,
+		/** Toggle button, triggers on the falling edge. */
+		TOGGLE_FALLING,
+	}
+
+	/**
+	 * Creates a new Button
+	 */
+	public Button() {
+		lastPressed = false;
+		pressed = false;
+		state = false;
+	}
+
+	@Override
+	public void updateServer() {
+		pressed = pressedSub.get();
+		if (buttonMode == ButtonMode.TOGGLE_RISING || buttonMode == ButtonMode.TOGGLE_FALLING) {
+			if (pressed != lastPressed) {
+				if (lastPressed) {
+					if (buttonMode == ButtonMode.TOGGLE_FALLING) {
+						state = !state;
+					}
+				} else {
+					if (buttonMode == ButtonMode.TOGGLE_RISING) {
+						state = !state;
+					}
+				}
+				lastPressed = pressed;
+			}
+		} else if (buttonMode == ButtonMode.PUSH) {
+			state = pressed;
+		}
+		statePub.set(state);
+	}
+
+	@Override
+	public void updateClient() {
+		state = stateSub.get();
+	}
+
+	@Override
+	public void setupNetworkTables(NetworkTable table) {
+		pressedTopic = table.getBooleanTopic("Pressed");
+		pressedPub = pressedTopic.publish();
+		pressedSub = pressedTopic.subscribe(false);
+		pressedPub.set(false);
+
+		stateTopic = table.getBooleanTopic("State");
+		statePub = stateTopic.publish();
+		stateSub = stateTopic.subscribe(false);
+	}
+
+	/**
+	 * Gets button state
+	 *
+	 * @return
+	 *         button state (true = pushed, false = released)
+	 */
+	public boolean getState() {
+		return state;
+	}
+
+	/**
+	 * Get a trigger for the button
+	 *
+	 * @return
+	 *         trigger
+	 */
+	public Trigger getTrigger() {
+		return new Trigger(() -> state);
+	}
+
+	/**
+	 * Sets the button's mode
+	 *
+	 * @param buttonMode
+	 *                mode to set the button to
+	 */
+	public void setMode(ButtonMode buttonMode) {
+		this.buttonMode = buttonMode;
+	}
+}
