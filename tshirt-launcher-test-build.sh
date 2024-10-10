b44f949fc96fff19b738abce50a21381f1929368
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-10-10 15:04:32 -0400

    Use the TShirtLauncher's buttonbox simulate script for test builds

--- tshirt-launcher-test-build.sh
+++ tshirt-launcher-test-build.sh
@@ -1,5 +1,4 @@
 ./gradlew build
 cp buttonbox-lib/build/libs/buttonbox-lib.jar ../TShirtLauncher/libs
-tmux \
-    new-session  'cd ../TShirtLauncher && ./gradlew simulateJava' \; \
-    split-window './gradlew :buttonbox-bridge:run'
+cd ../TShirtLauncher
+./buttonbox-simulate.sh
