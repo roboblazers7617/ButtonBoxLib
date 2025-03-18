dc2082803788888d4214d7631372dc2b0db82179
Apollo Nargang <CoffeeCoder1@outlook.com>
2025-03-17 22:14:32 -0400

    Use CoreMidi4J for MIDI communication Uses CoreMidi4J to get MIDI devices, fixing MIDI SysEx and some other things on OSX.

--- buttonbox-lib-midi/src/main/java/io/github/roboblazers7617/buttonbox/midi/MIDIUtil.java
+++ buttonbox-lib-midi/src/main/java/io/github/roboblazers7617/buttonbox/midi/MIDIUtil.java
@@ -4,8 +4,10 @@ import java.util.Optional;
 import java.util.Vector;
 
 import javax.sound.midi.MidiDevice;
-import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
+
+import uk.co.xfactorylibrarians.coremidi4j.CoreMidiDeviceProvider;
+import uk.co.xfactorylibrarians.coremidi4j.CoreMidiException;
 
 /**
  * Some static utility functions for working with MIDI devices.
@@ -25,27 +27,34 @@ public class MIDIUtil {
 	 *           returned.
 	 */
 	public static Optional<MIDIDevice> getDeviceByName(String rxName, String txName) {
-		MidiDevice device;
 		Vector<MidiDevice> recieverDevices = new Vector<MidiDevice>();
 		Vector<MidiDevice> transmitterDevices = new Vector<MidiDevice>();
-		MidiDevice.Info[] infos = MidiSystem.getMidiDeviceInfo();
 
-		// Find all devices that match the given conditions.
-		for (int i = 0; i < infos.length; i++) {
-			try {
-				device = MidiSystem.getMidiDevice(infos[i]);
+		try {
+			CoreMidiDeviceProvider midiDeviceProvider = new CoreMidiDeviceProvider();
 
-				if (device.getMaxReceivers() != 0 && infos[i].getName().equals(rxName)) {
-					recieverDevices.add(device);
+			MidiDevice.Info[] infos = midiDeviceProvider.getDeviceInfo();
+
+			// Find all devices that match the given conditions.
+			MidiDevice device;
+			for (int i = 0; i < infos.length; i++) {
+				try {
+					device = midiDeviceProvider.getDevice(infos[i]);
+
+					if (device.getMaxReceivers() != 0 && infos[i].getName().equals(rxName)) {
+						recieverDevices.add(device);
+					}
+
+					if (device.getMaxTransmitters() != 0 && infos[i].getName().equals(txName)) {
+						transmitterDevices.add(device);
+					}
+				} catch (IllegalArgumentException ex) {
+					// Handle or throw exception...
+					ex.printStackTrace();
 				}
-
-				if (device.getMaxTransmitters() != 0 && infos[i].getName().equals(txName)) {
-					transmitterDevices.add(device);
-				}
-			} catch (MidiUnavailableException ex) {
-				// Handle or throw exception...
-				ex.printStackTrace();
 			}
+		} catch (CoreMidiException ex) {
+			ex.printStackTrace();
 		}
 
 		// Make sure the devices exist, and, if they do, create a MIDIDevice with them and return that.
